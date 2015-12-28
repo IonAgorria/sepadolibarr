6e91b9c093866467e5ce0f65973c6d211cfcbae5
braito4 <braito4@hotmail.com>
2015-12-28 11:16:19 +0100

    Bug some Compatibility PHP 7

--- core/modules/modSepaDolibarr.class.php
+++ core/modules/modSepaDolibarr.class.php
@@ -158,7 +158,7 @@ class modSepaDolibarr extends DolibarrModules
      *      It also creates data directories.
      *      @return     int             1 if OK, 0 if KO
      */
-    function init()
+    function init($options = '')
     {
         $sql = array();
 
@@ -173,7 +173,7 @@ class modSepaDolibarr extends DolibarrModules
      *      Data directories are not deleted.
      *      @return     int             1 if OK, 0 if KO
      */
-    function remove()
+    function remove($options = '')
     {
         $sql = array();
 
